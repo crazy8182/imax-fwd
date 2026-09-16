bb8df124d38417926444a3649d9a7fc3ee44cb8d
crazy8182 <crazygaming4694@gmail.com>
2026-09-16 16:07:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "00")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "26741021")
+    API_HASH = environ.get("API_HASH", "7c5af0b88c33d2f5cce8df5d82eb2a94")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5672857559').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "vegamoviesforwordbot") 
 
     PICS = (environ.get('PICS', 'https://files.catbox.moe/uevfz8.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://devashibambhava0:devashibambhava0@cluster0.ux6amy9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002452671264'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Jisshubots") # FORCE SUB channel link 
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002084819782'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/imaxmoviehub") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
